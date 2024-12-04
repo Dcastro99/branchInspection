80236d41be882522bcfdc016bedfd6d37db6b200
Danny Castro <dcastro@gensco.com>
2024-12-04 10:20:39 -0800

    change ignore files

--- genscoSQLProject1/obj/Debug/net8.0/genscoSQLProject1.AssemblyInfo.cs
+++ genscoSQLProject1/obj/Debug/net8.0/genscoSQLProject1.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("genscoSQLProject1")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+80a471ba2f62c34a8c8dfea73da2d4977a3c3474")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+eb492e4522310d8b4168e4f90c4bef13320fc171")]
 [assembly: System.Reflection.AssemblyProductAttribute("genscoSQLProject1")]
 [assembly: System.Reflection.AssemblyTitleAttribute("genscoSQLProject1")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
