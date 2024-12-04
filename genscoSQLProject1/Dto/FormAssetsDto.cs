b955609127294575c557f3a1fd426075cd7ddf57
Danny Castro <dcastro@gensco.com>
2024-12-04 11:08:04 -0800

    removed file

--- genscoSQLProject1/Dto/FormAssetsDto.cs
+++
@@ -1,9 +0,0 @@
-﻿//namespace genscoSQLProject1.Dto
-//{
-//    public class FormAssetsDto
-//    {
-//        public int FormAssetsId { get; set; }
-//        public int BranchInspectionId { get; set; }
-//        public int AssetId { get; set; }
-//    }
-//}
