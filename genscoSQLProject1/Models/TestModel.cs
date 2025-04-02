5ba4b32d9979a4d5c9e9a95565942374b3b50938
Danny Castro <dcastro@gensco.com>
2025-04-02 09:49:04 -0700

    added testModel

---
+++ genscoSQLProject1/Models/TestModel.cs
@@ -0,0 +1,9 @@
+﻿namespace genscoSQLProject1.Models
+{
+    public class TestModel
+    {
+        public int TestModelId { get; set; }
+        public string TestModelName { get; set; }
+        public string TestModelDescription { get; set;
+    }
+}
